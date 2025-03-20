611c200a200bea64472ec3a2e51112770b3813da
Valor133 <j.lzicar@seznam.cz>
2025-03-20 21:05:32 +0100

    Update page.tsx

--- ronde-van-praageren/src/app/page.tsx
+++ ronde-van-praageren/src/app/page.tsx
@@ -148,7 +148,7 @@ export default function Home() {
           <div className="text-center mb-12">
             <h2 className="text-4xl font-bold mb-4 text-dark-blue">From the Race Director</h2>
             <p className="text-xl text-gray-600 max-w-3xl mx-auto">
-              A message about the spirit of Ronde van Praagerenkljblkjhlkjhlkjhlij
+              A message about the spirit of Ronde van Praagerenk
             </p>
           </div>
           
